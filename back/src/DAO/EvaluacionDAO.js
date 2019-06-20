4f101a4be8d7660364a3d191afd4e7d261066d28
elp4nsho <l.m_d.m@hotmail.com>
2019-06-20 04:56:31 -0400

    se agregan servicios de evaluacion

--- back/src/DAO/EvaluacionDAO.js
+++ back/src/DAO/EvaluacionDAO.js
@@ -316,13 +316,12 @@ exports.mostrarEvaluacionesRespondidas = async () => {
         sql = "SELECT * FROM usuariosAContestar";
         sql = "SELECT * FROM usuario INNER join usuariosAContestar on usuario.rut = usuariosAContestar.idUsuario";
         let usuariosAContestar = await devolverLaPromesaDeLaBaseDato(sql);
-        sql = "SELECT * FROM evaluacionAContestar INNER join evaluacion on evaluacion.id = evaluacionAContestar.idEvaluacion";
+        sql = "SELECT * FROM evaluacionAContestar";
         let evaluacionesAMostrar = await devolverLaPromesaDeLaBaseDato(sql);
 
 
         evaluacionesAMostrar.forEach( evaluacion => {
             evaluacionRespondida.idEvaluacionRespondida = evaluacion.id;
-            evaluacionRespondida.infoEvaluacion = evaluacion;
             evaluacionRespondida.fechaEvaluacionRespondida = evaluacion.fechaInicio;
             evaluacionRespondida.usuarios = [];
 
